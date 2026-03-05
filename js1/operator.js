c6f62ea46242d5aa02a5721f74d9308a728dc7af
Vikram Kumar <vsaw@gmail.com>
2026-03-06 02:44:44 +0530

    bit waise remain

--- js1/operator.js
+++ js1/operator.js
@@ -1,7 +1,36 @@
-//  ArithMatic operator 
-console.log(2+5);
-console.log(2 - 5);
-console.log(2 * 5);
-console.log(20/ 5);
-console.log(2 %5);
-console.log(2**10 );
+// //  ArithMatic operator 
+// console.log(2+5);
+// console.log(2 - 5);
+// console.log(2 * 5);
+// console.log(20/ 5);
+// console.log(2 %5);
+// console.log(2**10 );
+
+// // increment operator 
+// let num1= 23;
+// console.log(num1++) ;
+// console.log(num1);
+
+
+// assignment operator
+
+// let num2 = 6;
+// num2 +=6;
+// num2 -= 6;
+// num2 *= 6;
+// num2 /= 6;
+
+// console.log(num2); 
+// let num4 = 6;
+// num4 
+// *= 6;
+// console.log(num4); 
+
+// comparision operator 
+
+let num5=54;
+let num6 =54;
+console.log(num5 == num6);
+console.log(num5 !== num6);
+ console.log(num5 === num6);
+console.log(num5 > num6);
