0f46955671008e60e32d00ca99ac927f5fae866f
Vikram Kumar <vsaw@gmail.com>
2026-03-06 01:24:08 +0530

    type conversion

--- js1/typeConversion.js
+++ js1/typeConversion.js
@@ -4,3 +4,35 @@
 let num = 45;
 console.log(typeof String(num),num);
 console.log(typeof num.toString());
+
+// Boolean
+
+let boolean = false;
+console.log(typeof boolean);
+console.log(typeof String(boolean));
+
+// value to number
+
+//  string to number 
+
+let myName ="vikram";
+console.log(myName)
+console.log(typeof myName)
+console.log(typeof Number(myName), myName);
+
+
+// Boolean to number 
+
+let bool = true;
+console.log(typeof bool);
+console.log(typeof Number(bool));
+
+//  Explicit conversion 
+let num1 = 12+3+"vikram" ;
+console.log(typeof num1, num1);
+
+let num2 = "vikram"+12+2;
+console.log(typeof num2, num2);
+
+let num3 =14/"7";
+console.log(typeof num3 , num3) ;
