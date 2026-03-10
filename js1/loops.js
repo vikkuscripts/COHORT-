07b512c9297cfd4d14be4a9c0537d046659e3f7f
Vikram Kumar <vsaw@gmail.com>
2026-03-10 10:44:27 +0530

    loops 2 question

--- js1/loops.js
+++ js1/loops.js
@@ -1,14 +1,64 @@
-let users = ["vikram","ram","shyam","hari","gopal"];
+// let users = ["vikram","ram","shyam","hari","gopal"];
 
-// let totalUsers = users.length;
-
-
-for(let i=0; i<users.length; i++){
-    console.log(users[i]);
-} 
+// // let totalUsers = users.length;
 
 
-console.log(users[0]);
-console.log(users[1]);
-console.log(users[2]);
-console.log(users[3]);
+// for(let i=0; i<users.length; i++){
+//     console.log(users[i]);
+// } 
+
+
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+// console.log(users[3]);
+
+
+
+// Multiplication Table 
+
+function multiplicationTable(){
+    let number = 7;
+    console.log("Multiplication Table of " + number );
+
+    for(let i = 0; i <= 10 ; i++){
+        let result = number* i;
+        console.log(number + "x" + i + "=" + result);
+    }
+   console.log("loop finished");
+   console.log("table Printed ");
+
+   let message = "Parctice loops Daily ";
+   console.log(message);
+
+   let end = true;
+   if (end){
+     console.log('program end ');
+   }
+
+
+}
+multiplicationTable()
+
+
+function tbalePrint(){
+    let number = 9;
+    console.log("chosen number is " + number );
+
+    for(i=1; i <=10 ; i++){
+        let result = number * i ;
+        console.log ( number + "x" +i+ "="+ result);
+    }
+
+    console.log("loops finished ");
+    console.log("table printed ");
+
+    let meessage = "second parctice ";
+    console.log(meessage);
+
+    let end = true;
+    if (end){
+    console.log("program end ");
+  }
+}
+tbalePrint();
